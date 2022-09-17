fdd2c665dc6fa4ec2b7b70228f4648ed7194d196
TinStefanic <tin.stefanic@gmail.com>
2022-09-17 21:45:35 +0200

    Add project files.

---
+++ src/Settlements.Server/Data/SettlementsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Settlements.Shared.Models;
+
+namespace Settlements.Server.Data
+{
+	public class SettlementsContext : DbContext
+	{
+		public DbSet<Settlement> Settlements { get; set; } 
+
+		public SettlementsContext(DbContextOptions<SettlementsContext> options) : base(options)
+		{
+		}
+	}
+}
---
+++ src/Settlements.Shared/Models/Settlement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Settlements.Shared.Models
+{
+	public class Settlement
+	{
+		[Required]
+		public string? Country { get; set; }
+
+		[Required]
+		public string? Name { get; set; }
+
+		[Required]
+		public string? PostalCode { get; set; }
+	}
+}
